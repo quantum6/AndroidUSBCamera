9284a6b196c05a60a3313b17fedda038b11a5a15
quantum6 <quantum6@yeah.net>
2020-08-30 08:20:27 +0800

    REMOVE CODE

--- AndroidUSBCamera/app/src/main/java/gittest/uvc/amos/codes/com/uvcgittest/MainActivity.java
+++ AndroidUSBCamera/app/src/main/java/gittest/uvc/amos/codes/com/uvcgittest/MainActivity.java
@@ -1,37 +1,24 @@
 package gittest.uvc.amos.codes.com.uvcgittest;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.hardware.usb.UsbDevice;
-import android.os.Build;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import com.amos.codes.uvc.FileUtils;
 import com.amos.codes.uvc.UVCCameraHelper;
 import com.serenegiant.usb.CameraDialog;
 import com.serenegiant.usb.USBMonitor;
 import com.serenegiant.usb.common.AbstractUVCCameraHandler;
-import com.serenegiant.usb.encoder.RecordParams;
 import com.serenegiant.usb.widget.CameraViewInterface;
 import com.serenegiant.usb.widget.UVCCameraTextureView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements CameraDialog.CameraDialogParent, CameraViewInterface.Callback{
     private RelativeLayout relativeVideo;
@@ -72,49 +59,10 @@ public class MainActivity extends AppCompatActivity implements CameraDialog.Came
             showShortMsg("disconnecting");
         }
     };
-    String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    List<String> mPermissionList = new ArrayList<>();
-    private static final int PERMISSION_REQUEST = 1;
-    private void checkPermission() {
-        mPermissionList.clear();
-        //判断哪些权限未授予
-        for (int i = 0; i < permissions.length; i++) {
-            if (ContextCompat.checkSelfPermission(this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
-                mPermissionList.add(permissions[i]);
-            }
-        }
-        /**
-         * 判断是否为空
-         */
-        if (mPermissionList.isEmpty()) {//未授予的权限为空，表示都授予了
 
-        } else {//请求权限方法
-            String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);//将List转为数组
-            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST);
-        }
-    }
-    /**
-     * 响应授权
-     * 这里不管用户是否拒绝，都进入首页，不再重复申请权限
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSION_REQUEST:
-
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                break;
-        }
-    }
     private void showShortMsg(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,33 +70,9 @@ public class MainActivity extends AppCompatActivity implements CameraDialog.Came
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); //全屏
-        hideBottomUIMenu();  //隐藏虚拟按键
-        checkPermission();  //检查权限
+        //hideBottomUIMenu();  //隐藏虚拟按键
+        //checkPermission();  //检查权限
         initTextureViewSurface();  //初始化播放器控件
-
-
-    }
-    protected void hideBottomUIMenu() {
-        //隐藏虚拟按键，并且全屏
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-            View v = this.getWindow().getDecorView();
-            v.setSystemUiVisibility(View.GONE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-
-            Window _window = getWindow();
-            WindowManager.LayoutParams params = _window.getAttributes();
-            params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_IMMERSIVE;
-            _window.setAttributes(params);
-        }
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        /*
-        // step.2 register USB event broadcast
-        if (mCameraHelper != null) {
-           mCameraHelper.registerUSB();
-        }*/
     }
 
     @Override
@@ -163,10 +87,6 @@ public class MainActivity extends AppCompatActivity implements CameraDialog.Came
         }
     }
 
-    public boolean isCameraOpened() {
-        return mCameraHelper.isCameraOpened();
-    }
-
     @Override
     public void onSurfaceCreated(CameraViewInterface view, Surface surface) {
     }
@@ -178,10 +98,6 @@ public class MainActivity extends AppCompatActivity implements CameraDialog.Came
     @Override
     public void onSurfaceDestroy(CameraViewInterface view, Surface surface) {
     }
-
-
-
-
 
     /**
      * 初始化播放器的Surface
@@ -202,9 +118,6 @@ public class MainActivity extends AppCompatActivity implements CameraDialog.Came
         params.width = 1024;
         relativeVideo.setLayoutParams(params);
 
-
-
-
         uvcCameraTextureView = new UVCCameraTextureView(this);
         uvcCameraTextureView.setLayoutParams(params);
         relativeVideo.addView(uvcCameraTextureView);
@@ -221,7 +134,6 @@ public class MainActivity extends AppCompatActivity implements CameraDialog.Came
         if (mCameraHelper != null) {
             mCameraHelper.registerUSB();
         }
-
     }
 
 }
